0abd182823e8ba2cfb6f34e5f6cf6b40a9631134
markstownsend <markstownsend@yahoo.com>
2016-09-23 06:38:38 +0100

    decyper

--- wonderland-fsharp-katas/alphabet-cipher.fsx
+++ wonderland-fsharp-katas/alphabet-cipher.fsx
@@ -3,16 +3,20 @@
 type Message = string
 type Keyword = string
 
+// constant of the character array
 let cAlphabet = [|'a';'b';'c';'d';'e';'f';'g';'h';'i';'j';'k';'l';'m';'n';'o';'p';'q';'r';'s';'t';'u';'v';'w';'x';'y';'z'|]
-    
+
+// converts the char array to a string, nice use of fold
 let charArrayAsString (seed:char[])  = 
     let conv = seed |> Array.map(fun f -> f.ToString())
     conv |> Array.fold(+) ""
 
+// switches the line around to effectively rotate
 let rotatedLine line start = 
     let oldOrder = line |> Array.splitAt start
     fst oldOrder |> Array.append (snd oldOrder)
 
+// creates the substitution array
 let substitutionChart (seed:char[]) = 
     let substC = seed |> Array.mapi(fun i c -> rotatedLine seed i)
     substC
@@ -33,21 +37,18 @@ let rec padSeed (filled:char[]) (seed:char[]) (current:int) (max:int) =
             filled.[current] <- seed.[seedIndex]
             padSeed filled seed (current + 1) max
 
-//let adjustKey (key:string) (target:int) = 
-//    let lengthAdjustment = target - key.Length
-//    match lengthAdjustment <= 0 with
-//    | true -> key.Substring(0, key.Length + lengthAdjustment).ToCharArray()
-//    | false -> fillArray Array.create 'x' key 0 target 
-
+// gets the index of the character in an alphabet array
 let alphabetIndex (findChar:char) (findIn:char[]) = 
     findIn |> Array.findIndex (fun x -> x = findChar)
 
+// finds the character at the character co-ordinates in the alphabet square
 let findMe (substitutionSquare:char[][]) (x:char) (y:char) = 
     let topLine = substitutionSquare.[0]
     let iX = alphabetIndex x topLine
     let iY = alphabetIndex y topLine
     substitutionSquare.[iX].[iY]
 
+// encodes
 let encode (key:Keyword) (message:Message) : Message =
     let lenMsg = message.Length
     let filledKey = padSeed (Array.create lenMsg 'x') (key.ToCharArray()) 0 lenMsg
@@ -56,9 +57,17 @@ let encode (key:Keyword) (message:Message) : Message =
     let encoded = Array.map2 (fun x y -> (findMe cipherSquare x y)) filledKey cMsg
     charArrayAsString encoded
 
+// decodes - not working, develop this next
 let decode (key:Keyword) (message:Message) : Message =
-    "decodeme"
+    let lenMsg = message.Length
+    let filledKey = padSeed (Array.create lenMsg 'x') (key.ToCharArray()) 0 lenMsg
+    let cMsg = message.ToCharArray()
+    let cipherSquare = substitutionChart cAlphabet
+    let encoded = Array.map2 (fun x y -> (findMe cipherSquare y x)) filledKey cMsg
+    charArrayAsString encoded
 
+
+// what is difference between decode and decipher
 let decipher (cipher:Message) (message:Message) : Keyword =
     "decypherme"
 
@@ -73,7 +82,7 @@ let tests () =
     test <@ encode "scones" "meetmebythetree" = "egsgqwtahuiljgs" @>
 //
 //    // verify decoding
-//    test <@ decode "vigilance" "hmkbxebpxpmyllyrxiiqtoltfgzzv" = "meetmeontuesdayeveningatseven" @>
+    test <@ decode "vigilance" "hmkbxebpxpmyllyrxiiqtoltfgzzv" = "meetmeontuesdayeveningatseven" @>
 //    test <@ decode "scones" "egsgqwtahuiljgs" = "meetmebythetree" @>
 //
 //    // verify decyphering
@@ -106,4 +115,5 @@ let tests () =
 
 // run the tests
 tests ()
+
 
